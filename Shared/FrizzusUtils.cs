ba310798d2ed6bd6bfeda300eced9cdd2c497f8f
Frizzus <loiccanin.pro@gmail.com>
2023-02-10 17:05:57 +0100

    add util func, failed to use F#, used C# instead

---
+++ Shared/FrizzusUtils.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using System.Collections;
+
+static class FrizzusUtils
+{
+    public static IDictionary<string, string> getEnvArray(string envUrl)
+    {
+        string[] envFile = File.ReadAllLines(Directory.GetCurrentDirectory() + envUrl);
+        IDictionary<string,string> envArray = new Dictionary<string,string>();
+        foreach (var line in envFile)
+        {
+            string[] buffer = line.Split('=');
+            envArray.Add(buffer[0], buffer[1]);
+        }
+        return envArray;
+    }
+}
