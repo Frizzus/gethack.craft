9c5c54f03b68f3128784ccd0cd26bc8e725ce7ec
Frizzus <loiccanin.pro@gmail.com>
2023-02-14 09:05:39 +0100

    documented DBObject interface

--- Shared/DBObject.cs
+++ Shared/DBObject.cs
@@ -1,5 +1,17 @@
 interface DBObject{
+    /// <summary>
+    /// Delete the object's related record from the database,  the C# object is not deleted !
+    /// </summary>
+    /// <returns>True if the record has been deleted, else false</returns>
     public bool DeleteFromDB();
+    /// <summary>
+    /// Update the object's related record from the database
+    /// </summary>
+    /// <returns>True if the record has been updated, else false</returns>
     public bool UpdateToDB();
+    /// <summary>
+    /// Insert a record corresponding to an object
+    /// </summary>
+    /// <returns>True if the record has been created, else false</returns>
     public bool ConstructForDB();
 }
