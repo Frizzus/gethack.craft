e82dc192846fcabfe8ac8748a18be1b7d2d140e7
Frizzus <loiccanin.pro@gmail.com>
2023-02-16 14:00:51 +0100

    Comment classe done

--- Component/Comment.razor.cs
+++ Component/Comment.razor.cs
@@ -15,24 +15,17 @@ public class Comment : DBObject{
     /// <param name="user">The user who pubished this comment</param>
     /// <param name="hack">The post where the comment has been published</param>
     /// <param name="content">The content of the comment</param>
-    public Comment(int user, int hack, string content){
+    public Comment(int user, int hack, string content, MySqlCommand request){
 
         this.relatedUserId = user;
         this.relatedHackId = hack;
         this.content = content;
 
-        if (!this.ConstructForDB())
+        if (!this.ConstructForDB(request))
         {
             throw new Exception("The insertion of the object in the Database has failed");
         }
         // Get the id and the SQL server current date the DB give with AUTO-INCREMENT and then update the object with it
-
-        IDictionary<string,string> dotEnv = FrizzusUtils.getEnvArray(@"\.env");
-        MySqlConnection connection = new MySqlConnection($"server={dotEnv["DB_HOST"]};userid={dotEnv["DB_USER"]};password={dotEnv["DB_PASSWORD"]};database={dotEnv["DB_DATABASE"]};");
-        connection.Open();
-
-        MySqlCommand request = new MySqlCommand();
-        request.Connection = connection;
             
         // Getting the database id and date based on the most recent date from this.relatedUser
         request.CommandText = "SELECT id_comment, last_updated FROM Comment WHERE last_updated = (SELECT MAX(last_updated) FROM Comment WHERE id_user = @relatedUser AND id_hack = @relatedHack)";
@@ -43,8 +36,6 @@ public class Comment : DBObject{
         
         this.id = data.GetInt32(0);
         this._lastUpdated = data.GetDateTime(1);
-
-        connection.Close();
     }
 
     public Comment(int id, MySqlCommand request){
@@ -63,22 +54,14 @@ public class Comment : DBObject{
 
     public DateTime LastUpdated {get;}
 
-    public bool DeleteFromDB(){
-        IDictionary<string,string> dotEnv = FrizzusUtils.getEnvArray(@"\.env");
+    public bool DeleteFromDB(MySqlCommand request){
         try
         {
-            MySqlConnection connection = new MySqlConnection($"server={dotEnv["DB_HOST"]};userid={dotEnv["DB_USER"]};password={dotEnv["DB_PASSWORD"]};database={dotEnv["DB_DATABASE"]};");
-            connection.Open();
-
-            MySqlCommand request = new MySqlCommand();
-            request.Connection = connection;
             request.CommandText = "DELETE FROM Comment WHERE id_comment = @id";
             request.Parameters.AddWithValue("@id", this.id);
             request.Prepare();
 
             request.ExecuteNonQuery();
-
-            connection.Close();
 
             return true;
         }
@@ -88,16 +71,9 @@ public class Comment : DBObject{
         }
     }
 
-    public bool UpdateToDB(){
-        IDictionary<string,string> dotEnv = FrizzusUtils.getEnvArray(@"\.env");
+    public bool UpdateToDB(MySqlCommand request){
         try
         {
-            MySqlConnection connection = new MySqlConnection($"server={dotEnv["DB_HOST"]};userid={dotEnv["DB_USER"]};password={dotEnv["DB_PASSWORD"]};database={dotEnv["DB_DATABASE"]};");
-            connection.Open();
-
-            MySqlCommand request = new MySqlCommand();
-            request.Connection = connection;
-
             request.CommandText = "UPDATE Comment SET content = @content, nb_likes = @nbLikes";
             request.Parameters.AddWithValue("@content", this.content);
             request.Parameters.AddWithValue("@nbLikes", this.nbLikes);
@@ -105,8 +81,6 @@ public class Comment : DBObject{
             
             request.ExecuteNonQuery();
 
-            connection.Close();
-
             return true;
         }
         catch (System.Exception)
@@ -115,19 +89,9 @@ public class Comment : DBObject{
         }
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <returns></returns>
-    public bool ConstructForDB(){
-        IDictionary<string,string> dotEnv = FrizzusUtils.getEnvArray(@"\.env");
+    public bool ConstructForDB(MySqlCommand request){
         try
         {
-            MySqlConnection connection = new MySqlConnection($"server={dotEnv["DB_HOST"]};userid={dotEnv["DB_USER"]};password={dotEnv["DB_PASSWORD"]};database={dotEnv["DB_DATABASE"]};");
-            connection.Open();
-
-            MySqlCommand request = new MySqlCommand();
-            request.Connection = connection;
 
             request.CommandText = "INSERT INTO Comment(content, nb_likes, id_hack, id_user) VALUES(@content, @nbLikes, @relatedHack, @relatedUser)";
             request.Parameters.AddWithValue("@content", this.content);
@@ -137,8 +101,6 @@ public class Comment : DBObject{
             request.Prepare();
 
             request.ExecuteNonQuery();
-
-            connection.Close();
 
             return true;
         }
--- Shared/DBObject.cs
+++ Shared/DBObject.cs
@@ -1,17 +1,18 @@
+using MySql.Data.MySqlClient;
 interface DBObject{
     /// <summary>
     /// Delete the object's related record from the database,  the C# object is not deleted !
     /// </summary>
     /// <returns>True if the record has been deleted, else false</returns>
-    public bool DeleteFromDB();
+    public bool DeleteFromDB(MySqlCommand request);
     /// <summary>
     /// Update the object's related record from the database
     /// </summary>
     /// <returns>True if the record has been updated, else false</returns>
-    public bool UpdateToDB();
+    public bool UpdateToDB(MySqlCommand request);
     /// <summary>
     /// Insert a record corresponding to an object
     /// </summary>
     /// <returns>True if the record has been created, else false</returns>
-    public bool ConstructForDB();
+    public bool ConstructForDB(MySqlCommand request);
 }
